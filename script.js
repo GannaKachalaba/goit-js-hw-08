d2f7ae2a53b6fab4173734671bdc7da212699103
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-02-25 19:01:35 +0200

    lesson 12
    lesson 12

--- script.js
+++ script.js
@@ -1,237 +1,90 @@
-// *//* Example 1 - Коллбек функції
-// Напишіть наступні функції:
+// document.body.addEventListener("keydown", handleKeydown);
 
-//? createProduct(obj, callback) - приймає об'єкт товару без id, а також коллбек. Функція створює об'єкт товару, додаючи йому унікальний ідентифікатор у властивість id та викликає коллбек передаючи йому створений об'єкт.
-
-//? logProduct(product) - колббек приймаючий об'єкт продукту і логуючий його в консоль
-
-//? logTotalPrice(product) - колббек, що приймає об'єкт продукту і логіює загальну вартість товару в консоль*
-
-// const productList = [
-//   {
-//     name: "banana",
-//     amount: 5,
-//     price: 10,
-//   },
-//   {
-//     name: "apple",
-//     amount: 10,
-//     price: 8,
-//   },
-//   {
-//     name: "cherry",
-//     amount: 15,
-//     price: 13,
-//   },
-// ]; 
-
-// function createProduct(obj, callback) {
-  // // 1. приймає обʼєкт і приймає колбек
-  // // 2. створення нового обʼєкту і додавання властивості айді
-  // // 3. виклик колбек функції з передачою
-  //        туди новостворенного 
-    
-    // console.log(callback);
-
-// let callback = function logTotalPrice(product) {
-// console.log(product.amount * product.price);
-//  }
-
-
-//   const newObj = { ...obj, id: Date.now() };
-//   callback(newObj);
+// function handleKeydown(event) {
+//   console.log(event);
 // }
 
-// function logProduct(product){  
-//     console.log(product);
-// }
+/*
+ ? TASK
+*
+* 1. Створити модальне вікно яке буде відкриватися при кліку на кнопку всередині тега body. Модальне вікно має закриватися при кліку на напівпрозорий оверлей, та на іконку хрестика всередині модалки
+*
+* 2. Додати в модалку логіку закриття при натисканні на кнопку **Escape**
+*/
 
-// logProduct(productList[0]);
+// const modalWindow = document.getElementById("modalWindow");
+// const overlay = document.getElementById("overlay");
+// const closeBtn = document.getElementById("closeBtn");
+// const openModalBtn = document.getElementById("openModalBtn");
+// const modalOpenClass = "modal-open";
 
+// openModalBtn.addEventListener("click", handleModalOpen);
+// overlay.addEventListener("click", handleModalOpen);
+// closeBtn.addEventListener("click", handleModalOpen);
 
-// function logTotalPrice(product) {
-//     console.log(product.amount * product.price);
-// }
+// document.body.addEventListener("keydown",
+//     handleModalCloseOnESC, {
+//         capture: true,
+//         once: true,
+//         passive: true
+//     });
 
-// createProduct(productList[2], logProduct);
-// createProduct(productList[2], logTotalPrice);
-// createProduct(productList[1], logTotalPrice);
-
-// /////////////////////////////////////////////////////////
-//* Example 2 - Коллбек функції
-
-//? Додайте об'єкт account методи withdraw(amount, onSuccess, onError) та deposit(amount, onSuccess, onError), де перший параметр це сума операції, а другий та третій - коллбеки.
-
-//? Метод withdraw викликає onError якщо amount більше TRANSACTION_LIMIT або this.balance, і onSuccess в іншому випадку.
-
-//? Метод deposit викликає onError якщо amount більше TRANSACTION_LIMIT або менше або дорівнює нулю, і onSuccess в іншому випадку.
-
-// const TRANSACTION_LIMIT = 1000;
-
-// const account = {
-//     username: "Alex",
-//     balance: 2000,
-//     withdraw(amount, onSuccess, onError) {
-//         if (amount > this.balance) {
-//             onError(
-//               `На жаль, на вашому балансі недостатньо коштів! Поточний баланс: ${this.balance}`
-//             );
-//         } else if (amount >= TRANSACTION_LIMIT) {
-//             onError(
-//               `На жаль, ви перевищили ліміт! Поточний ліміт: ${TRANSACTION_LIMIT}`
-//             );
-//         } else {
-//             this.balance -= amount;
-//             onSuccess(`Гроші знято! Поточний баланс: ${this.balance}`);
-//         }
-//     },
-//     deposit(amount, onSuccess, onError) {
-//         if (amount <= 0) {
-//           onError("Сума транзакції повинна бути більша нуля!");
-//         } else if (amount >= TRANSACTION_LIMIT) {
-//           onError(
-//             `На жаль, ви перевищили ліміт! Поточний ліміт: ${TRANSACTION_LIMIT}`
-//           );
-//         } else {
-//           this.balance += amount;
-//           onSuccess(
-//             `Гроші покладено на рахунок! Поточний баланс: ${this.balance}`
-//           );
-//         }
-//     },
+// function handleModalCloseOnESC({ key }) {
+//     console.log(key);
+//     if (key === "Escape" &&
+//         document.body.classList.contains(modalOpenClass)) {
+//         handleModalOpen();
+//   }
 // }
 
 
-
-
-// function onSuccess(message) {
-//    console.log(`Успішна операція! ${message}`); 
+// function handleModalOpen() {
+//     document.body.classList.toggle(modalOpenClass);
 // }
-    
-// function onError(message) {
-//   console.log(`Помилка! ${message}`);
-// }
+// ////////////////////////////////////////////////////
 
-// account.withdraw(5000, onSuccess, onError);
-// account.withdraw(1500, onSuccess, onError);
-// account.withdraw(950, onSuccess, onError);
-// account.deposit(10000, onSuccess, onError);
-// account.deposit(-5, onSuccess, onError);
-// account.deposit(300, onSuccess, onError);
+// const container = document.getElementById("container");
+// console.log(container.childNodes);
 
+// //////////////////////////////////////////////////
 
-// /////////////////////////////////////////////////////////////////////////////////////////
+// const form = document.querySelector(".form");
 
-//* Example 3 - Коллбек функції
-// Напишіть функцію each(array, callback), яка першим параметром очікує масив, а другим - функцію, яка застосовується до кожного елемента масиву. Функція each повинна повернути новий масив, елементами якого будуть результати виклику коллбека.
+// form.addEventListener("submit", handleSubmit);
 
-// function each(array, callback) {
-//   //1. Створимо новий масив
-//   //2. перебрати массив array за допомогою циклу
-//   //3. виклик колбек функції на кожному елементі масиву array
-//   //4. додоємо видозмінений елемент в новий масив
-//   //5. повернути новий масив
-    
-//     const modifiedArr = [];
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const { elements: { login, password } } = event.currentTarget;
+//     console.log(event.currentTarget);
 
-//     for (const el of array) {
-//         const modifiedEl = callback(el);  
-//         modifiedArr.push(modifiedEl);
+//     if (login.value === "" || password.value === "") {
+//         return console.log("Please fill in all the fields!");
 //     }
-//     return modifiedArr;
+// }
+// ////////////////////////////////////////////////////
+//  function foo() {
+//   alert(`"hello" ${this.name}`);
 // }
 
-
-// const arr = [1, 2, 3, 4, 5];
-
-// console.log(each(arr, pow));
-// console.log(each(arr, addOne));
-// console.log(each(arr, (num) => num / 2));
-
-// function pow(num) {
-//     return num ** 2;
-// }
-
-// function addOne(num) {
-//   return num + 1;
-// }
-
-// //////////////////////////////////////////////
-
-//* Example 4 - Стрілочні функції
-// Виконайте рефакторинг коду за допомогою стрілочних функцій.
-
-// const productList = [
-//   {
-//     name: "Banana",
-//     amount: 5,
-//     price: 10,
-//   },
-//   {
-//     name: "Apple",
-//     amount: 10,
-//     price: 8,
-//   },
-//   {
-//     name: "Cherry",
-//     amount: 15,
-//     price: 13,
-//   },
-// ];
-
-// //   function createProduct(obj, callback) {
-// //   const newObj = { ...obj, id: Date.now() };
-// //   callback(newObj);
-// // }
-
-// const createProduct = (obj, callback) => {
-//     const newObj = { ...obj, id: Date.now() };
-//     callback(newObj);
+// const params = {
+//   handleEvent: foo,
+//   name: "Alex",
 // };
 
-// // const createProduct = (obj, callback) => callback({...obj, id:
-// // Date.now()});
+// document.body.addEventListener("click", params);
 
-// const logProduct = (product) => console.log(product);
-// const logTotalPrice = (product) => console.log(product.amount * product.price);
+// setTimeout(() => {
+//     document.body.removeEventListener("click", params);
+// }, 3000);
 
-// createProduct(productList[2], logProduct);
-// createProduct(productList[2], logTotalPrice);
-// createProduct(productList[1], logTotalPrice);
+// //////////////////////////////////////////////////////
 
-// ////////////////////////////////////////////////////////////////////////////////////
+// const link = document.getElementById("link");
 
-// function showArgs() {
-//     console.log(arguments);
-// }
+// link.addEventListener("click", (e) => {
+//     e.preventDefault();
+// });
 
-// showArgs(1, 5, 8, 13, 2);
-
-// ///////////////////////////////////////////////////////////////////////////////////
-
-//* Example 9 - Метод forEach
-// Виконайте рефакторинг коду за допомогою методу forEach та стрілочні функції.
-
-function calсulateAverage(...args) {
-  let total = 0;
-    // for (let i = 0; i < args.length; i++) {
-    //   total += args[i];
-    // }
-
-    args.forEach(el => (total += el));
- 
-  return total / args.length;
-}
-
-console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
-console.log(calсulateAverage(14, 8, 2)); // 8
-console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
-
-
-
-
-
-
-
+// ///////////////////////////////////////////////////
 
 
