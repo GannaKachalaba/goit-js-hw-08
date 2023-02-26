0e5fa1384223ca333df731d08fc52555a5627465
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-02-26 16:13:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,53 +21,64 @@
 
 // При завантаженні сторінки сприпт має автоматично розрахувати вартість на основі даних за замовчуванням які ви встановите в розмітці.
 
-const form = document.getElementById("form");
-const price = document.getElementById("price");
-const quantity = document.getElementById("quantity");
-const amount = document.getElementById("amount");
-const total = document.getElementById("total");
+// const form = document.getElementById("form");
+// const price = document.getElementById("price");
+// const quantity = document.getElementById("quantity");
+// const amount = document.getElementById("amount");
+// const total = document.getElementById("total");
 
-const data = {
-    price: 0,
-    quantity: 0,
-    calcTotalPrice() {
-        return (this.price * this.quantity).toFixed(2);
-    },
-};
+// const data = {
+//     price: 0,
+//     quantity: 0,
+//     calcTotalPrice() {
+//         return (this.price * this.quantity).toFixed(2);
+//     },
+// };
 
-// data fill
-dataFill();
-displayTotalPrice();
+// // data fill
+// dataFill();
+// displayTotalPrice();
 
-// events listener
+// // events listener
 
-form.addEventListener("change", handleForChange);
+// form.addEventListener("change", handleForChange);
 
-// functions
+// // functions
 
-function handleForChange({ target }) {
+// function handleForChange({ target }) {
 
-    const { value } = target;
+//     const { value } = target;
 
-    if (target === quantity) {
-        // data.quantity = value;
-        amount.textContent = value;
-    }
-    // else if (target === price) {
-    //     data.price = value;
-    // }
+//     if (target === quantity) {
+//         // data.quantity = value;
+//         amount.textContent = value;
+//     }
+//     // else if (target === price) {
+//     //     data.price = value;
+//     // }
 
-    target.setAttribute("value", value);
-    dataFill();
-    displayTotalPrice();
-    console.log(data);
-}
+//     target.setAttribute("value", value);
+//     dataFill();
+//     displayTotalPrice();
+//     console.log(data);
+// }
 
-function dataFill(){
-    data.price = price.getAttribute("value");
-    data.quantity = quantity.getAttribute("value");
-}
+// function dataFill(){
+//     data.price = price.getAttribute("value");
+//     data.quantity = quantity.getAttribute("value");
+// }
 
-function displayTotalPrice() {
-    total.textContent = data.calcTotalPrice() + " грн";
-}
+// function displayTotalPrice() {
+//     total.textContent = data.calcTotalPrice() + " грн";
+// }
+// /////////////////////////////////////////////////
+
+const button = document.getElementById("button");
+const box = document.getElementById("box");
+
+
+box.addEventListener("click", (event) => {
+    console.log(event.target);
+    console.log(event.currentTarget);
+    console.dir(event.target);
+})
